29ccde1132f4281cd68fdf1debad2be17f1d2945
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-06-17 23:08:12 +0800

    Random dice on press working

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.green,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          centerTitle: true,
+          title: Text('Dice One!'),
+          backgroundColor: Colors.lightGreen,
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,42 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int dice1 = Random().nextInt(6) + 1;
+  int dice2 = Random().nextInt(6) + 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  dice1 = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  dice2 = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
