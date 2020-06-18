d144527625ea9ab81f85908b3e24fc300cb6527f
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-06-18 12:32:39 +0800

    Fixed landscape view

--- lib/main.dart
+++ lib/main.dart
@@ -26,36 +26,50 @@ class _DicePageState extends State<DicePage> {
   int dice1 = Random().nextInt(6) + 1;
   int dice2 = Random().nextInt(6) + 1;
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                buttonClicked();
-              },
-              child: Image.asset('images/dice$dice1.png'),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                buttonClicked();
-              },
-              child: Image.asset('images/dice$dice2.png'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   void buttonClicked() {
     setState(() {
       dice1 = Random().nextInt(6) + 1;
       dice2 = Random().nextInt(6) + 1;
     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      buttonClicked();
+                    },
+                    child: Image.asset('images/dice$dice1.png'),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      buttonClicked();
+                    },
+                    child: Image.asset('images/dice$dice2.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Text(
+              'Rane',
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
